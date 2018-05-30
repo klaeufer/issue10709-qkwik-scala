83e0637cafb31670ecb369f3dd8898e8697d1369
John O'Sullivan <josullivan1@luc.edu>
2018-05-30 18:18:31 -0500

    pull spy wrapper into def it

--- src/test/scala/FurtherSpyExplorations2.scala
+++ src/test/scala/FurtherSpyExplorations2.scala
@@ -97,11 +97,11 @@ class FurtherSpyExplorations2 extends AssertionsForJUnit with MockitoSugar {
 
   @Test def noExcessiveHasNextInJoinIterator_SpyIterateSeparately: Unit = {
     val exp = List(1, 2, 3, 1, 2, 3)
-    def it = Iterator(1, 2, 3)
+    def it = spy(Iterator(1, 2, 3))
     // Iterate separately
-    val i0 = spy(it)
+    val i0 = it
     val r0 = i0.toList
-    val i1 = spy(it)
+    val i1 = it
     val r1 = i1.toList
     assertSameElements(exp, r0 ++ r1)
     verify(i0, times(4)).hasNext
@@ -110,11 +110,11 @@ class FurtherSpyExplorations2 extends AssertionsForJUnit with MockitoSugar {
 
   @Test def noExcessiveHasNextInJoinIterator_SpyJoinSeparately: Unit = {
     val exp = List(1,2,3,1,2,3)
-    def it = Iterator(1,2,3)
+    def it = spy(Iterator(1,2,3))
     // JoinIterator
     val res = new mutable.ArrayBuffer[Int]
-    val i0 = spy(it)
-    val i1 = spy(it)
+    val i0 = it
+    val i1 = it
     (i0 ++ i1).foreach(res += _)
     assertSameElements(exp, res)
     verify(i0, times(4)).hasNext
@@ -123,11 +123,11 @@ class FurtherSpyExplorations2 extends AssertionsForJUnit with MockitoSugar {
 
   @Test def noExcessiveHasNextInJoinIterator_SpyConcatSeparately: Unit = {
     val exp = List(1,2,3,1,2,3)
-    def it = Iterator(1,2,3)
+    def it = spy(Iterator(1,2,3))
     // ConcatIterator
     val res = new mutable.ArrayBuffer[Int]
-    val i0 = spy(it)
-    val i1 = spy(it)
+    val i0 = it
+    val i1 = it
     (Iterator.empty ++ i0 ++ i1).foreach(res += _)
     assertSameElements(exp, res)
     verify(i0, times(4)).hasNext
